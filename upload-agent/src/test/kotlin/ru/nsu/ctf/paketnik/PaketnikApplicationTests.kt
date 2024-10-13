3cf6c3b089b2d2b6c611bf4b4b0c960a77968bbe
Georgiy Vinokurov <113652537+g-vinokurov@users.noreply.github.com>
2024-10-13 15:44:10 +0700

    PAKETNIK-12654-init-project (#1)
    * Init Spring project for upload-agent
    * Fix artefactId + Add linter

---
+++ upload-agent/src/test/kotlin/ru/nsu/ctf/paketnik/PaketnikApplicationTests.kt
@@ -0,0 +1,11 @@
+package ru.nsu.ctf.paketnik
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaketnikApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
