c4554f2b896bf906ecdc9d8ba70fbbf3486ad5ad
Artyom <79071150+Artyom-Kitov@users.noreply.github.com>
2024-10-17 18:18:20 +0700

    12652 Init backend project (#4)

---
+++ backend/src/main/kotlin/ru/nsu/ctf/paketnikback/PaketnikBackApplication.kt
@@ -0,0 +1,11 @@
+package ru.nsu.ctf.paketnitkback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaketnikBackApplication
+
+fun main(args: Array<String>) {
+    runApplication<PaketnikBackApplication>(*args)
+}
