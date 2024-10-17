99b01b021df518e6a59a3fc2ef5448f75c7a7918
Artyom-Kitov <artem.k3790@gmail.com>
2024-10-17 16:40:21 +0700

    12652 Init backend project

---
+++ backend/src/test/kotlin/ru/nsu/ctf/paketnikback/PaketnikBackApplicationTests.kt
@@ -0,0 +1,11 @@
+package ru.nsu.ctf.paketnikback
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaketnikBackApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
