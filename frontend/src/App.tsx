5673e7cead9dd383af9c3f6f1193a98c6b05828a
Igor Comp <a.nazarov3@g.nsu.ru>
2024-10-23 01:10:38 +0700

    Task #12788 Install component library

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card"; 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex flex-col items-center min-h-screen">
+      <h1 className="text-4xl font-bold mb-8">Paketnik</h1>
+      <div className="w-4/5 max-w-7xl flex justify-center bg-gray-100 py-16">
+        <Card className="w-96 bg-white shadow-lg">
+          <CardHeader>
+            <CardTitle>Hello World</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Card content</p>
+          </CardContent>
+        </Card>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
