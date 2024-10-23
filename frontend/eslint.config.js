7305d868d5254bfe9dead13e0a5fe9e94e3d08aa
Dmitry Stonie <dmitrystonie@gmail.com>
2024-10-23 22:15:19 +0700

    Убрал coverages из .prettierignore, добавил скрипт lint:fix и format, отлинтил и отформатировал весь проект этими скриптами

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,33 +4,32 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-
-
 export default [
   {
-    files:
-        ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx,json}"],
   },
   {
-    languageOptions: { globals: globals.browser }
+    languageOptions: { globals: globals.browser },
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   eslintConfigPrettier,
   {
-    rules:
-    {
-      "no-restricted-exports": ["error", { "restrictDefaultExports": { "direct": true } }],
+    rules: {
+      "no-restricted-exports": [
+        "error",
+        { restrictDefaultExports: { direct: true } },
+      ],
       "arrow-parens": ["error", "always"],
       "@typescript-eslint/no-explicit-any": "error",
       "no-restricted-syntax": [
         "error",
         {
-          "selector": "TSEnumDeclaration",
-          "message": "Don't declare enums"
-        }
-      ]
-    }
-  } 
+          selector: "TSEnumDeclaration",
+          message: "Don't declare enums",
+        },
+      ],
+    },
+  },
 ];
