c48ac16ffba94b6d9b0a8470d5bb360f50488de0
Mark Yurin <112599768+DmitryStonie@users.noreply.github.com>
2024-10-29 17:47:06 +0700

    Paketnik 12791 linters configuration (#8)
    * Init eslint, add prettier, resolve conflicts in it's rules configs
    
    * Add eslint rules, add typescript rules, it works
    
    * Убрал coverages из .prettierignore, добавил скрипт lint:fix и format, отлинтил и отформатировал весь проект этими скриптами
    
    * исключил директорию dist из eslint
    
    * format project

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,12 +2,37 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx,json}"],
+  },
+  {
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    ignores: ["**/dist/"],
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
+  {
+    rules: {
+      "no-restricted-exports": [
+        "error",
+        { restrictDefaultExports: { direct: true } },
+      ],
+      "arrow-parens": ["error", "always"],
+      "@typescript-eslint/no-explicit-any": "error",
+      "no-restricted-syntax": [
+        "error",
+        {
+          selector: "TSEnumDeclaration",
+          message: "Don't declare enums",
+        },
+      ],
+    },
+  },
 ];
