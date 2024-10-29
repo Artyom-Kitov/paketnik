db0fde1e5959658692131a5fb09653d6adc997b1
Igor Comp <a.nazarov3@g.nsu.ru>
2024-10-29 22:28:09 +0700

    Added concrete stream widget

---
+++ frontend/src/fixtures/streamData.ts
@@ -0,0 +1,46 @@
+export const streamData = [
+  {
+    id: 1,
+    serviceName: "sheetty", 
+    srcIp: "10.10.16.6",
+    srcPort: 51058,
+    dstIp: "10.10.10.2",
+    dstPort: 1337,
+    protocol: "TCP",
+    startTime: "18:24:52",
+    duration: "1.07s",
+    up: "100", 
+    down: "1.2K", 
+    rules: ["flag_out", "flag_in"],
+    headers: {
+      ethernet: {
+        srcMac: "AA:BB:CC:DD:EE:FF",
+        destMac: "FF:EE:DD:CC:BB:AA",
+      },
+      ip: {
+        version: 4,
+        packetLength: 1500,
+        flags: { doNotFragment: 1, moreFragments: 0 },
+        fragmentOffset: 0,
+        ttl: 64,
+        protocol: "TCP",
+        headerChecksum: "0x1A2B",
+        srcIp: "10.10.16.6",
+        destIp: "10.10.10.2",
+      },
+      tcp: {
+        srcPort: 51058,
+        destPort: 1337,
+        sequenceNumber: 12345,
+        ackNumber: 67890,
+        dataOffset: 5,
+        flags: { SYN: true, ACK: false, FIN: false },
+        windowSize: 1024,
+        checksum: "0x1A2B",
+        urgentPointer: 0,
+        payload: "Example Payload",
+      },
+    },
+  },
+];
+  
