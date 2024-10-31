f2130ba5b3168b4d9bc2e51216ccebadf2d2f306
Shaskels <allasidner028@gmail.com>
2024-10-31 18:40:46 +0700

    Внесла правки

--- frontend/src/components/ui/streamsList/StreamsListWidget.tsx
+++ frontend/src/components/ui/streamsList/StreamsListWidget.tsx
@@ -1,34 +1,49 @@
 import React, { useState } from "react";
-import "./StreamsListWidget.css"
 
-const StreamsListWidget = () => {
-    const [streams, setStream] = useState([])
+export const StreamsListWidget = () => {
+  const [streams, setStream] = useState([]);
 
-    return (
-        <div className="streamsList">        
-            <div className="title">Streams</div>
-            <table className="table">
-                <thead className="tableHead">
-                    <tr>
-                        <th>service</th>
-                        <th>srcip</th>
-                        <th>srcport</th>
-                        <th>dstip</th>
-                        <th>dstport</th>
-                        <th>started_at</th>
-                        <th>duration</th>
-                        <th>up</th>
-                        <th>down</th>
-                    </tr>
-                </thead>
-            </table>
-            <div className="streamsContainer"> 
-                {
-                    streams.map((stream) => <div></div>)
-                }
-            </div>
-        </div>
-    );
-}
-
-export default StreamsListWidget
+  return (
+    <div className="w-full h-fit">
+      <div className="text-right text-[#fff] text-2xl font-bold">Streams</div>
+      <table className="w-full mb-[12px]">
+        <thead className="bg-[#475569] h-[50px] w-full">
+          <tr>
+            <th className="text-xl text-[#e2e8f0] font-bold sticky px-20.45">
+              service
+            </th>
+            <th className="text-xl text-[#e2e8f0] font-bold sticky px-20.45">
+              srcip
+            </th>
+            <th className="text-xl text-[#e2e8f0] font-bold sticky px-20.45">
+              srcport
+            </th>
+            <th className="text-xl text-[#e2e8f0] font-bold sticky px-20.45">
+              dstip
+            </th>
+            <th className="text-xl text-[#e2e8f0] font-bold sticky px-20.45">
+              dstport
+            </th>
+            <th className="text-xl text-[#e2e8f0] font-bold sticky px-20.45">
+              started_at
+            </th>
+            <th className="text-xl text-[#e2e8f0] font-bold sticky px-20.45">
+              duration
+            </th>
+            <th className="text-xl text-[#e2e8f0] font-bold sticky px-20.45">
+              up
+            </th>
+            <th className="text-xl text-[#e2e8f0] font-bold sticky px-20.45">
+              down
+            </th>
+          </tr>
+        </thead>
+      </table>
+      <div className="h-[870px] bg-[#475569]">
+        {streams.map((stream) => (
+          <div></div>
+        ))}
+      </div>
+    </div>
+  );
+};
