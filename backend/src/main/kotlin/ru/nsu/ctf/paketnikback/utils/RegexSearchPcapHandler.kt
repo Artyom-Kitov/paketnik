625604a7ce3b76e5a5a26319062d3e8aae7a321e
g-vinokurov <g.vinokurov@g.nsu.ru>
2024-11-21 20:59:51 +0700

    Fix bugs, add regex finding

--- backend/src/main/kotlin/ru/nsu/ctf/paketnikback/utils/RegexSearchPcapHandler.kt
+++ backend/src/main/kotlin/ru/nsu/ctf/paketnikback/utils/RegexSearchPcapHandler.kt
@@ -6,24 +6,30 @@ import io.pkts.packet.Packet
 import io.pkts.packet.PCapPacket
 
 class RegexSearchPcapHandler : PacketHandler {
-    private var matches : List<Triple<Int, String, Int>>
+    private var matches : mutableListOf<Triple<Int, String, Int>>()
     private var regex : Regex
+    private var packetsCounter : Int = 0
     
     constructor(_regex : Regex) {
-        matches = listOf<Triple<Int, String, Int>>()
         regex = _regex
     }
     
     fun getMatches() : List<Triple<Int, String, Int>> {
-        return matches
+        return matches.toList()
     }
 
     override fun nextPacket(packet : Packet) : Boolean {
 
-        val pcapPacket = (PCapPacket) packet.getPacket(Protocol.PCAP)
-        val buffer = pcapPacket.getPayload()
+        val pcapPacket : PCapPacket = packet.getPacket(Protocol.PCAP)
+        val buffer : ByteArray = pcapPacket.getPayload()
+        val text = buffer.toString(Charsets.UTF_8)
         
-        // TODO: Implement finding matches
+        val items = regex.findAll(text)
+        for (item in items) {
+            matches.add(Triple(packetsCounter, item.value, item.range.first))
+        }
+        
+        packetsCounter = packetsCounter + 1
 
         // Return true if you want to keep receiving next packet
         // Return false if you want to stop traversal
