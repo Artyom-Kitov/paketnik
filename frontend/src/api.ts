b4ad5ce4958797a98be9c650d180c92b258d2345
Dmitry Stonie <dmitrystonie@gmail.com>
2024-11-24 13:27:56 +0700

    изменил метод fetchData

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -12,33 +12,49 @@ export async function getServices(): Promise<Service[]> {
 }
 
 export async function updateService(service: Service): Promise<void> {
-  return await fetchData(host + "/services/", {
-    method: "PUT",
-    headers: {
+  return await fetchData(
+    host + "/services/",
+    "PUT",
+    {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(service),
-  });
+    JSON.stringify(service),
+  );
 }
 
 export async function deleteService(id: string): Promise<void> {
-  return await fetchData(host + "/service/" + id, {
-    method: "DELETE",
-  });
+  return await fetchData(
+    host + "/service/" + id,
+    "DELETE",
+    {
+      "Content-Type": "application/json",
+    },
+    "",
+  );
 }
 
 export async function postService(service: Service): Promise<void> {
-  return await fetchData(host + "/services", {
-    method: "POST",
-    headers: {
+  return await fetchData(
+    host + "/services",
+    "POST",
+    {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(service),
-  });
+    JSON.stringify(service),
+  );
 }
 
-async function fetchData(input: string, init: RequestInit): Promise<void> {
-  return await fetch(input, init)
+async function fetchData(
+  path: string,
+  method: string,
+  headers: { "Content-Type": string },
+  body: string,
+): Promise<void> {
+  return await fetch(path, {
+    method: method,
+    headers: headers,
+    body: body,
+  })
     .then((response) => response.json())
     .then(({ success }) => {
       if (!success) {
