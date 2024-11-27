64edba4935b90217ecf5ea9e92222b7d162eb822
Borisov Dmitry <101406982+rtatrol@users.noreply.github.com>
2024-11-27 14:20:44 +0700

    add hash validation (#35)

--- backend/src/main/kotlin/ru/nsu/ctf/paketnikback/controller/config/MinioConfig.kt
+++ backend/src/main/kotlin/ru/nsu/ctf/paketnikback/controller/config/MinioConfig.kt
@@ -1,6 +1,9 @@
 package ru.nsu.ctf.paketnikback.controller.config
 
+import io.minio.BucketExistsArgs
+import io.minio.MakeBucketArgs
 import io.minio.MinioClient
+import io.minio.errors.MinioException
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -8,9 +11,36 @@ import org.springframework.context.annotation.Configuration
 class MinioConfig {
     @Bean
     fun minioClient(): MinioClient {
-        return MinioClient.builder()
+        val client = MinioClient
+            .builder()
             .endpoint("http://localhost:9000")
             .credentials("admin", "password")
             .build()
+
+        val bucketName = "default-bucket"
+
+        try {
+            val found = client.bucketExists(
+                BucketExistsArgs
+                    .builder()
+                    .bucket(bucketName)
+                    .build(),
+            )
+            if (!found) {
+                client.makeBucket(
+                    MakeBucketArgs
+                        .builder()
+                        .bucket(bucketName)
+                        .build(),
+                )
+                println("Default bucket $bucketName успешно создан")
+            } else {
+                println("Default bucket $bucketName уже существует")
+            }
+        } catch (e: MinioException) {
+            println("Ошибка при создании default bucket: ${e.message}")
+        }
+
+        return client
     }
 }
