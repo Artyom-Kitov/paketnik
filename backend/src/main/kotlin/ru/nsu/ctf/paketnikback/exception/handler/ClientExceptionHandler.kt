ccbc959bdbc9e042f3204a1a1d7e5b6bfd8a2456
Artyom <79071150+Artyom-Kitov@users.noreply.github.com>
2024-12-03 12:04:17 +0700

    Fix contest service tests (#42)
    * PAKETNIK-13778: add autotests
    
    * 13962 fixed tests, exposed docker-compose ports, added default credentials for minio and mongodb
    
    ---------
    
    Co-authored-by: v.stepanyuk <v.stepanyuk>

--- backend/src/main/kotlin/ru/nsu/ctf/paketnikback/exception/handler/ClientExceptionHandler.kt
+++ backend/src/main/kotlin/ru/nsu/ctf/paketnikback/exception/handler/ClientExceptionHandler.kt
@@ -1,26 +1,34 @@
 package ru.nsu.ctf.paketnikback.exception.handler
 
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 import ru.nsu.ctf.paketnikback.exception.EntityNotFoundException
 import ru.nsu.ctf.paketnikback.utils.logger
 
 @ControllerAdvice
-class ExceptionHandler {
+class ClientExceptionHandler : ResponseEntityExceptionHandler() {
     private val log = logger()
-
-    @ExceptionHandler(MethodArgumentNotValidException::class)
-    fun handleValidationException(e: MethodArgumentNotValidException): ResponseEntity<String> {
-        log.error("invalid request", e)
-        return ResponseEntity.badRequest().body(e.message)
-    }
 
     @ExceptionHandler(EntityNotFoundException::class)
     fun handleEntityNotFoundException(e: EntityNotFoundException): ResponseEntity<String> {
-        log.error("entity not found", e)
+        log.error(e.message)
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.message)
+    }
+    
+    override fun handleMethodArgumentNotValid(
+        ex: MethodArgumentNotValidException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        log.error(ex.message)
+        return ResponseEntity.badRequest().body(ex.message)
     }
 }
