019ebe54917224ec6d480cc503e770eb66707bce
Shaskels <allasidner028@gmail.com>
2024-12-12 00:56:53 +0700

    api для удаления и получения pcap по id

--- frontend/src/components/ui/widgets/FilesWidget.tsx
+++ frontend/src/components/ui/widgets/FilesWidget.tsx
@@ -1,8 +1,52 @@
 import { useState } from "react";
-
+import { getPcap, deletePcap, getPcapById } from "../../../api";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function FilesWidget() {
-    const [selectedFile, setSelectedFile] = useState<undefined|number>(undefined)
+    const [selectedFile, setSelectedFile] = useState<undefined|string>(undefined)
+    const { isPending, isError, data, error } = useQuery({
+        queryKey: ["pcaps"],
+        queryFn: getPcap,
+      });
+    const queryClient = useQueryClient();
+    const deletePcapMutation = useMutation({
+        mutationFn: deletePcap,
+        onSuccess: () => {
+          queryClient.invalidateQueries({ queryKey: ["pcaps"] });
+        },
+    });
+    const getPcapByIdMutation = useMutation({
+        mutationFn: getPcapById,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["pcaps"] });
+          },
+    })
+
+    function onAnalysisClick(id : undefined|string){
+        if (id != undefined){
+            getPcapByIdMutation.mutate(id)
+        }
+    }
+
+    function onDeleteClick(id : undefined|string){
+        if (id != undefined){
+            deletePcapMutation.mutate(id)
+        }
+    }      
+
+    if (isPending) {
+        <div className="w-full h-full flex flex-col">
+          <div className="text-right text-[#fff] text-2xl font-bold mb-2">
+            Loading... {data}
+          </div>
+        </div>;
+    } else if (isError) {
+        <div className="w-full h-full flex flex-col">
+          <div className="text-right text-[#fff] text-2xl font-bold mb-2 text-red-600">
+            Error: {error.message}
+          </div>
+        </div>;
+    }
 
     return (
         <div className="w-full h-full flex flex-col">
@@ -11,25 +55,25 @@ export function FilesWidget() {
             </div>
             <div className="bg-[#475569] p-4 text-white flex-1 overflow-auto">
                 <div className="space-y-4">
-                    <div className="bg-[#2d3748] p-4 rounded-lg">
-                        <h3 className="text-lg font-semibold mb-3">PCAP Dumps</h3>
+                    <div className="bg-[#2d3748] w-fit p-4 rounded-lg">
+                        <h3 className="text-lg  font-semibold mb-3">PCAP Dumps</h3>
                         <div className="space-y-2">
-                            {[{id : 1, name : 'capture1.pcap'}, {id : 2, name : 'capture2.pcap'}, {id : 3, name : 'capture3.pcap'}].map((file) => (
+                            {data?.map((file) => (
                                 <div 
                                 key={file.id} 
                                 onClick={() => setSelectedFile(file.id)}  
                                 style={{background: selectedFile==file.id ? '#4a5568' : '#1e293b'}}
                                 className="flex items-center hover:!bg-[#4a5568] justify-between p-2 rounded"
                                 >
-                                    <span>{file.name}</span>
+                                    <span>{file.id}</span>
                                 </div>
                             ))}
                         </div>
                         <div className="space-x-2 mt-2">
-                            <button className="px-3 py-1 bg-[#4a5568] rounded hover:bg-[#2d3748] transition-colors">
+                            <button onClick={()=> onAnalysisClick(selectedFile)}  className="px-3 py-1 bg-[#4a5568] rounded hover:bg-[#2d3748] transition-colors">
                                 Analysis
                             </button>
-                            <button className="px-3 py-1 bg-[#e53e3e] rounded hover:bg-[#c53030] transition-colors">
+                            <button onClick={()=> onDeleteClick(selectedFile)} className="px-3 py-1 bg-[#e53e3e] rounded hover:bg-[#c53030] transition-colors">
                                 Delete
                             </button>
                         </div>
