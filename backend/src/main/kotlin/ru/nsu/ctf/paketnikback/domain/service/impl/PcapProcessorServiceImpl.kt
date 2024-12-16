dbd56a419f9ff1a168fbfec21bb696067124af9f
g-vinokurov <g.vinokurov@g.nsu.ru>
2024-12-16 21:06:21 +0700

    Init Pcap Processor

---
+++ backend/src/main/kotlin/ru/nsu/ctf/paketnikback/domain/service/impl/PcapProcessorServiceImpl.kt
@@ -0,0 +1,15 @@
+package ru.nsu.ctf.paketnikback.domain.service.impl
+
+import org.springframework.stereotype.Service
+
+import ru.nsu.ctf.paketnikback.domain.service.PcapProcessorService
+
+import ru.nsu.ctf.paketnikback.utils.logger
+
+@Service
+class PcapProcessorServiceImpl(
+) : PcapProcessorService {
+    private val log = logger()
+
+    // TODO: need to be implemented
+}
