e2b5c72e66c0ff564ae2218c6b66fdab81693918
g-vinokurov <g.vinokurov@g.nsu.ru>
2024-12-24 13:01:18 +0700

    Remove unused import

--- backend/src/main/kotlin/ru/nsu/ctf/paketnikback/domain/service/PcapProcessorService.kt
+++ backend/src/main/kotlin/ru/nsu/ctf/paketnikback/domain/service/PcapProcessorService.kt
@@ -1,7 +1,6 @@
 package ru.nsu.ctf.paketnikback.domain.service
 
 import ru.nsu.ctf.paketnikback.domain.dto.RegexSearchMatch
-import ru.nsu.ctf.paketnikback.domain.entity.packet.PacketData
 
 interface PcapProcessorService {
     fun searchByRegex(pcapId: String, regex: Regex): List<RegexSearchMatch>
