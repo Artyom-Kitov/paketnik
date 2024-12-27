00fafd9c5e4cb0f3357096b3bca8fd07ac318bc7
Ivan Baksheev <zpix-dev@list.ru>
2024-12-27 12:23:02 +0700

    fix: services

--- frontend/src/components/ui/streamInfoWidget/StreamInfoWidget.tsx
+++ frontend/src/components/ui/streamInfoWidget/StreamInfoWidget.tsx
@@ -37,6 +37,7 @@ export const StreamInfoWidget: React.FC = () => {
     },
   });
 
+
   // Determine the left side IP by finding the first packet with IPv4 layer
   const leftSideIp = useMemo(() => {
     if (!data?.length) {
